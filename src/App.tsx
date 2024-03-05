767b0e737e481b8694da1c23c73ce0e81c66cd58
Gabriel Rossetto <rossetto.gabriel.santos@gmail.com>
2024-03-05 02:45:18 -0300

    feat(app): add notfound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import './index.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
